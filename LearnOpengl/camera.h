97fee38a8fb5da9deb469364390a7ce95e8f9d3e
Cars Bells <cars.bells@outlook.com>
2026-07-07 22:33:39 -0600

    Camera Exercise 1.2
    -Same thing, 0 as y vector on keypress for w and s

--- LearnOpengl/camera.h
+++ LearnOpengl/camera.h
@@ -73,9 +73,9 @@ public:
     void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
         float velocity = MovementSpeed * deltaTime;
         if (direction == FORWARD)
-            Position += Front * velocity;
+            Position += glm::vec3(Front.x, 0.0f, Front.z) * velocity; //Make y constant to keep movement along xz plane
         if (direction == BACKWARD)
-            Position -= Front * velocity;
+            Position -= glm::vec3(Front.x, 0.0f, Front.z) * velocity; // ^^^^
         if (direction == LEFT)
             Position -= Right * velocity;
         if (direction == RIGHT)
